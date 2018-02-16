36bf9fd745e3042e1895f23c37cedbabb158d89b
Utilisateur <user@debian.arcahe.ovh>
2018-02-16 18:29:54 +0100

    ajout de ClientServeur

---
+++ ClientServeur/Client.java
@@ -0,0 +1,27 @@
+
+/**
+ * Décrivez votre classe Client ici.
+ * Classe implémentant un client.
+ * @author (Jean Destribois)
+ * @version (16/02/2018)
+ */
+public class Client
+{
+    private String nom;
+    
+    /**
+     * Constructeur d'objets de classe Client
+     */
+    public Client(String nom)
+    {
+        this.nom = nom;
+    }
+    /**
+     * Méthode permettant de connecter le client au serveur
+     */
+    public bool seConnecter(Serveur serveur)
+    {
+    
+    }
+    
+}
